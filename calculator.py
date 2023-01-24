35b14a0e136d615aa852dd9863db4b9d02a4e724
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-01-24 11:53:06 +0100

    first commit

--- calculator.py
+++ calculator.py
@@ -1,20 +1,5 @@
-def add(a, b):
-    return a+b+1
-def sub(a, b):
-    return a-b+1
-def mul(a, b):
-    return a * b
-def div(a, b):
-    return a / b
-def mod(a, b):
-    return a % b
-def squ(a, b):
-    return a  ** b
-a = int(input())
-b = int(input())
-print(add(a, b))
-print(sub(a, b))
-print(mul(a, b))
-print(div(a, b))
-print(mod(a, b))
-print(squ(a, b))
+def add(a,b):
+    return a+b
+
+a,b = 5,6
+print(add(a,b))

4e2546e9a2942b10f9f4bd5339c9f6ecd11e2c2c
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-01-24 09:07:04 +0100

    adding division logic

--- calculator.py
+++ calculator.py
@@ -4,8 +4,11 @@ def sub(a, b):
     return a-b
 def mul(a, b):
     return a * b
+def div(a, b):
+    return a / b
 a = int(input())
 b = int(input())
 print(add(a, b))
 print(sub(a, b))
 print(mul(a, b))
+print(div(a, b))
